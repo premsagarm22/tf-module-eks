2aa3334d2d20072197ddf8dc1d3a67df93bd6f73
premsarja <premsagar@prodot.in>
2024-04-06 09:20:00 +0530

    done

--- data.tf
+++ data.tf
@@ -1,9 +1,9 @@
 data "terraform_remote_state" "vpc" {
-    backend = "s3"
-    config ={
-      
-      bucket = "premms"
-      key    = "vpc/${var.ENV}/terraform.tfstate"
-      region = "us-east-1"
-    }
+  backend = "s3"
+
+  config = {
+    bucket = "premms"
+    key    = "vpc/${var.ENV}/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
